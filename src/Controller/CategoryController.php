4aac29dd48f9f04af5b85c32bad5ff687e2f0d78
devJeremie <chabanaisjeremie@gmail.com>
2024-05-31 15:09:10 +0200

    CategoryController

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(CategoryRepository $repo, ): Response
     {
+        $categories = $repo->findAll();//permet de recuperer tout les éléments de la bdd 
+
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories //contient un array avec toutes les catégories
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function addCategory(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+           // dd($category);
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+       
+        return $this->render('category/newCategory.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+
+    #[Route('/category/{id}/update ', name: 'app_category_update')]
+    public function updateCategory(Category $category, EntityManagerInterface $entityManager, Request $request): Response
+    {
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+        }
+       
+        return $this->render('category/updateCategory.html.twig',[
+            'form'=>$form->createView()
         ]);
     }
 }
