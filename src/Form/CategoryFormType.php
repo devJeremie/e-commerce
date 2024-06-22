ada21704c2e40e8ee887442dc8bf412e3776ec48
devJeremie <chabanaisjeremie@gmail.com>
2024-06-22 10:30:42 +0200

    navbar

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,7 @@ class CategoryFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name');     
     }
 
     public function configureOptions(OptionsResolver $resolver): void
