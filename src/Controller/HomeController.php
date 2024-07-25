2802e773a23464357e5286de592ff93f5271dc11
devJeremie <chabanaisjeremie@gmail.com>
2024-07-25 11:21:01 +0200

    ShowSubCategoryProduct

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -35,10 +35,10 @@ class HomeController extends AbstractController
     }
 
     #[Route('/product/subcategory/{id}/filter ', name: 'app_home_product_filter', methods: ['GET'])]
-    public function filter($id, SubCategoryRepository $subCategoryRepository): Response //ici on recupere 
+    public function filter($id, SubCategoryRepository $subCategoryRepository): Response //ici on recupere l'id et la repo des sous catégories
     
     {
-      
+        $product = $subCategoryRepository->find($id)->getProducts(); //on a crée une variable qui contient le repo des sous-catégories que l'on récup grace a leur id et on recherche les produits qui sont reliés
         return $this->render('home/filter.html.twig', [ //il faut bien sur créer ce fichier
            
         ]);
