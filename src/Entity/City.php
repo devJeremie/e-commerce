c3b6ab8b04007dbecfee7929edbcfe8bba33d784
devJeremie <chabanaisjeremie@gmail.com>
2024-08-02 18:37:38 +0200

    >CityErrorCreatedAt

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CityRepository::class)]
@@ -18,6 +20,22 @@ class City
 
     #[ORM\Column]
     private ?float $shippingCost = null;
+
+    /**
+     * @var Collection<int, Order>
+     */
+    #[ORM\OneToMany(targetEntity: Order::class, mappedBy: 'city')]
+    private Collection $orders;
+
+    /**
+     * @var Collection<int, Order>
+     */
+   
+    public function __construct()
+    {
+        $this->createdAt = new ArrayCollection();
+        $this->orders = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -47,4 +65,35 @@ class City
 
         return $this;
     }
+
+    /**
+     * @return Collection<int, Order>
+     */
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function addOrder(Order $order): static
+    {
+        if (!$this->orders->contains($order)) {
+            $this->orders->add($order);
+            $order->setCity($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrder(Order $order): static
+    {
+        if ($this->orders->removeElement($order)) {
+            // set the owning side to null (unless already changed)
+            if ($order->getCity() === $this) {
+                $order->setCity(null);
+            }
+        }
+
+        return $this;
+    }
+
 }
