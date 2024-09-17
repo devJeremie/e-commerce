094e307aa1ef9db0dd25add117657d21b1ffcba8
devJeremie <chabanaisjeremie@gmail.com>
2024-09-17 13:52:54 +0200

    PrintBillPdf

--- src/Controller/BillController.php
+++ src/Controller/BillController.php
@@ -18,11 +18,18 @@ class BillController extends AbstractController
 
         $pdfOptions = new Options(); //definit la nouvelle instanciation de classe Options de Dompdf
         $pdfOptions->set('defaultFont','Arial'); //Définit la font
-        $domPdf = new Dompdf($pdfOptions);
+        $domPdf = new Dompdf($pdfOptions);//On ajoute les options
+        $html = $this->renderView('bill/index.html.twig', [
+            'order'=>$order,
+         ]); // On insere ce que l'on veut imprimer
+        $domPdf->loadHtml($html); // On charge le html dans dompdf
+        $domPdf->render(); //On crée le rendu
+        $domPdf->stream('SneakHub-Facture-'.$order->getId().'.pdf',[//On concatene la facture avec la terminaison"pdf"
+            'Attachment'=>false //ca permet de dire on va telecharger le fichier, ou l'afficher et decider de l'imprimer et telecharger
+        ]); 
         
-
-        return $this->render('bill/index.html.twig', [
-           'order'=>$order,
+        return new Response('',200,[ //
+            'Content-Type' => 'application/pdf' 
         ]);
     }
 }
