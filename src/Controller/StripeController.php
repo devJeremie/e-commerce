1471a73bfce56ac1e81a4a50f3e2ca25e75c5137
devJeremie <chabanaisjeremie@gmail.com>
2024-09-21 19:43:29 +0200

    StripeNotifyMaj

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Stripe\Stripe;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StripeController extends AbstractController
 {
@@ -22,5 +24,38 @@ class StripeController extends AbstractController
         return $this->render('stripe/index.html.twig', [
             'controller_name' => 'StripeController',
         ]);
+    }
+
+    #[Route('/stripe/notify', name: 'app_stripe_notify')]
+    public function stripeNotify(Request $request): Response
+    {
+        Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY']); //recupère la cle secrete dans le fichier .env gràce au $_server
+        
+        $endpoint_secret= 'whsec_762838b6a470d9e24ef2fd08b52ec325225147e39308429459f540637fc2205b'; //cle stripe
+        $payload = $request->getContent();
+        $sigHeader = $request->headers->get('Stripe-Signature');
+        $event = null;
+
+        try{
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sigHeader, $endpoint_secret
+            );
+        }catch(\UnexpectedValueException $e) {
+            return new Response('Invalid payload', 400);
+        }catch(\Stripe\Exception\SignatureVerificationException $e){
+            return new Response('Invalid signature', 400);
+        }
+        switch($event->type){
+            case 'payment_intent.succeeded':
+                $paymentIntent = $event->data->object;
+                $fileName = 'stripe-detail-'.uniqid().'txt';
+                file_put_contents($fileName, $paymentIntent);
+                break;
+            case 'payment_method.attached':
+                $paymentMethod = $event->data->object;
+                break;
+            default :
+                break;
+        }
     }
 }
