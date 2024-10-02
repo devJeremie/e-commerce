610c151fc3f2d6c1713d2232252143c0703c47d9
devJeremie <chabanaisjeremie@gmail.com>
2024-10-02 10:35:04 +0200

    SearchEngineMaj

--- src/Controller/SearchEngineController.php
+++ src/Controller/SearchEngineController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,16 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SearchEngineController extends AbstractController
 {
     #[Route('/search/engine', name: 'app_search_engine', methods: ['POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, ProductRepository $productRepository): Response
     {
 
-        if ($request->isMethod('POST')){
+         // Vérifie si la requête est de type POST
+         if ($request->isMethod('POST')){
+            // Récupère les données de la requête
             $data = $request->request->all();
-            $word = $data['search'];
+            // Récupère le mot-clé de recherche
+            $word = $data['word'];
+            
+            // Appelle la méthode searchEngine du repository pour récupérer les résultats de recherche
+            $results = $productRepository->searchEngine($word);
         }
 
+        // Rendu de la vue search_engine/index.html.twig avec les résultats de recherche
         return $this->render('search_engine/index.html.twig', [
-            'controller_name' => 'SearchEngineController',
+            'products' => $results,
         ]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 //find($id, $lockMode = null, $lockVersion = null)
-//findOneBy(array $criteria, array $orderBy = null)
+//findBy(array $criteria, array $orderBy = null)
 //findAll()
 //findOneBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
 
@@ -21,17 +21,28 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    public function searchEngine(string $query){
-        return $this->createQueryBuilder('p')
-            ->where('p.name LIKE :query')//query est ici une variable le nom est arbitraire
-            ->orWhere('p.description LIKE :query')
-            ->setParameter('query','%' .$query.'%')
-            ->getQuery()
-            ->getResult();
+ /**
+ * Recherche dans la base de données les éléments qui correspondent à la requête de recherche.
+ *
+ * @param string $query La requête de recherche à exécuter.
+ * 
+ * @return array Les résultats de la recherche.
+ */
+public function searchEngine(string $query) {
+    // Crée un objet de requête qui permet de construire la requête de recherche.
+    return $this->createQueryBuilder('p')
+        // Recherche les éléments dont le nom contient la requête de recherche.
+        ->where('p.name LIKE :query')
+        // OU recherche les élées dont la description contient la requête de recherche.
+        ->orWhere('p.description LIKE :query')
+        // Défini la valeur de la variable "query" pour la requête.
+        ->setParameter('query', '%' . $query . '%')
+        // Exécute la requête et récupère les résultats.
+        ->getQuery()
+        ->getResult();
+}
 
-    }
-
-            //methode que lon peut créer soi meme
+           // methode que lon peut créer soi meme
     //    /**
     //     * @return Product[] Returns an array of Product objects
     //     */
