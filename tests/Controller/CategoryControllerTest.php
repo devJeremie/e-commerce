4784676d8919eb5d18daf3d7ceae93aaafb86d9a
devJeremie <chabanaisjeremie@gmail.com>
2025-12-17 14:57:17 +0100

    End-Test-Category

--- tests/Controller/CategoryControllerTest.php
+++ tests/Controller/CategoryControllerTest.php
@@ -2,13 +2,28 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CategoryControllerTest extends WebTestCase
 {
-    public function testIndexPageLoadsForAuthenticatedUser(): void
+
+    public function testRedirectsToLoginWhenUnauthenticated(): void
     {
         $client = static::createClient();
+        $client->request('GET', '/admin/category');
+        $this->assertResponseRedirects('/login');
+    }
+
+    public function testAuthenticatedAdminAccess(): void
+    {
+        $client = static::createClient();
+
+        // MOCK CategoryRepository
+        $categoryRepoMock = $this->createMock(CategoryRepository::class);
+        $categoryRepoMock->method('findAll')->willReturn([]);
+        static::getContainer()->set(CategoryRepository::class, $categoryRepoMock);
+
         $container = static::getContainer();
         $userProvider = $container->get('security.user.provider.concrete.app_user_provider_test');
         $user = $userProvider->loadUserByIdentifier('test@test.com');
@@ -18,9 +33,15 @@ class CategoryControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    public function testAddCategoryPageLoadsForAuthenticatedUser(): void
+    public function testAddFormRequiresAdmin(): void
     {
         $client = static::createClient();
+
+        // MOCK CategoryRepository
+        $categoryRepoMock = $this->createMock(CategoryRepository::class);
+        $categoryRepoMock->method('findAll')->willReturn([]);
+        static::getContainer()->set(CategoryRepository::class, $categoryRepoMock);
+
         $container = static::getContainer();
         $userProvider = $container->get('security.user.provider.concrete.app_user_provider_test');
         $user = $userProvider->loadUserByIdentifier('test@test.com');
