382196117ec31ab4c3030c6154304cbc1423e4fd
devJeremie <chabanaisjeremie@gmail.com>
2026-03-13 08:26:33 +0100

    maj

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends AbstractController
     public function showProduct(Product $product, ProductRepository $productRepository,CategoryRepository $categoryRepository): Response //ici on recupere directement l'entity Product,
     // le repository sert pour les derniers produits ajoutés
     {
-        $lastProductsAdd = $productRepository->findBy([],['id'=>'DESC'],5);//on crée la variable a laquelle on donne le repo et lam ethode findBy, puis on donne une limit de 5 en affichage
+        $lastProductsAdd = $productRepository->findBy([],['id'=>'DESC'],5);//on crée la variable a laquelle on donne le repo et la methode findBy, puis on donne une limit de 5 en affichage
 
         return $this->render('home/show.html.twig', [ //il faut bien sur créer ce fichier
             'product'=>$product,
