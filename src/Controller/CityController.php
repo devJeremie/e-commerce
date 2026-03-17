e2163b5eb2e138b8d4cc88193748ed9c2ca72cd0
devJeremie <chabanaisjeremie@gmail.com>
2026-03-17 13:15:55 +0100

    Maj-CityController

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+//Réservé a l'admin ou role editor
 #[Route('/editor/city')]
 class CityController extends AbstractController
 {
